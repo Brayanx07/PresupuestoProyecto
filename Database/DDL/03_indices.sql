c36f5a364db28351321ab66cdad83b12d56227da
BrayanX750 <ninopinel@gmail.com>
2026-09-01 07:58:01 -0600

    creacion para indices

---
+++ Database/DDL/03_indices.sql
@@ -0,0 +1,8 @@
+-- Acelera los reportes que filtran un presupuesto en un mes especifico
+CREATE INDEX IDX_TRX_PRESUP_PERIODO ON TRANSACCION (ID_PRESUPUESTO, ANIO, MES);
+
+-- Acelera el calculo del monto ejecutado por subcategoria en un mes
+CREATE INDEX IDX_TRX_SUBCAT_PERIODO ON TRANSACCION (ID_SUBCATEGORIA, ANIO, MES);
+
+-- Acelera el balance mensual de ingresos, gastos y ahorros de un usuario
+CREATE INDEX IDX_TRX_USUARIO_PERIODO ON TRANSACCION (ID_USUARIO, ANIO, MES);
